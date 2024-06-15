aa430434d8cddd3c9465d40e9978a4e2ba777726
Suyash Sanjay Bhadgaonkar <ssuyash161971@gmail.com>
2024-06-15 22:39:56 +0530

    Express update

--- Express/MiniProject-Task-Manager/script.js
+++ Express/MiniProject-Task-Manager/script.js
@@ -11,9 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     fs.readdir(`./files`, (err, files) => {
-        console.log(files);
+        // console.log(files);
         res.render('index', { files: files });
     });
+})
+
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, data) => {
+        console.log(data);
+        res.render('show', { filedata: data, filename: req.params.filename });
+    })
+})
+
+app.get('/edit/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', (err, data) => {
+        console.log(data);
+        res.render('edit', { prevname: req.params.filename });
+    })
 })
 
 app.post('/create', (req, res) => {
@@ -21,6 +35,15 @@ app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, `${req.body.details}`, (err) => {
         if (err) console.log(err);
         else console.log("Successfully added");
+    })
+    res.redirect('/');
+})
+
+app.post('/update', (req, res) =>{
+    // console.log(req.body);
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err)=>{
+        if(err) console.log(err);
+        else console.log("renamed successfully");
     })
     res.redirect('/');
 })
